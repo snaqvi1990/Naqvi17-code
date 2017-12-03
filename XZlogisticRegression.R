d4abd3d5dd52a350ab41bdcfc90f7919df151fa3
snaqvi1990 <sahin.naqvi@gmail.com>
2017-12-03 12:27:50 -0500

    Add files via upload

--- XZlogisticRegression.R
+++ XZlogisticRegression.R
@@ -1,17 +1,20 @@
 # reads in Supplemental Table 3 or 4 (in txt format), and fits multinomial or binomial logistic regression models to X- or Z-linked genes, with or without 
-# Pct score as a predictor
+# Pct score or pHI as a predictor
 library(nnet)
 ancX_forlogit = read.table("PATH_TO_SUPPTABLE_S3",sep="\t",fill=TRUE,header = TRUE,stringsAsFactors = FALSE)
 ancX_forlogit$evo_class = NA
-ancX_forlogit[which(ancX_forlogit$gene %in% subset(ancX_forlogit,Human.Y==TRUE)$gene),"evo_class"] = "XY"
-ancX_forlogit[which(ancX_forlogit$gene %in% subset(ancX_forlogit,(Human.Y==FALSE)&(XCI.Status =="I"))$gene),"evo_class"] = "I"
-ancX_forlogit[which(ancX_forlogit$gene %in% subset(ancX_forlogit,(Human.Y==FALSE)&(XCI.Status  %in% c("E","VE")))$gene),"evo_class"] = "E"
-mn_x_nomir = multinom(evo_class~Haploinsufficiency.probability+Human.expression.breadth+Human-mouse.dN/dS,data=na.omit(ancX_forlogit[,c("evo_class","Haploinsufficiency.probability","Human.expression.breadth","Mean.Pct","Human-mouse.dN/dS")]))
-mn_x_pctmir = multinom(evo_class~Haploinsufficiency.probability+Human.expression.breadth+Human-mouse.dN/dS+Mean.Pct,data=na.omit(ancX_forlogit[,c("evo_class","Haploinsufficiency.probability","Human.expression.breadth","Mean.Pct","Human-mouse.dN/dS")]))
+ancX_forlogit[which(ancX_forlogit$Gene %in% subset(ancX_forlogit,Human.Y==TRUE)$Gene),"evo_class"] = "XY"
+ancX_forlogit[which(ancX_forlogit$Gene %in% subset(ancX_forlogit,(Human.Y==FALSE)&(XCI.status..Balaton.2016. =="I"))$Gene),"evo_class"] = "I"
+ancX_forlogit[which(ancX_forlogit$Gene %in% subset(ancX_forlogit,(Human.Y==FALSE)&(XCI.status..Balaton.2016.  %in% c("E","VE")))$Gene),"evo_class"] = "E"
+ancX_forlogit$evo_class = as.factor(ancX_forlogit$evo_class)
+mn_x_nomir = multinom(evo_class~Haploinsufficiency.probability+Human.expression.breadth+Human.mouse.dN.dS,data=na.omit(ancX_forlogit[,c("evo_class","Haploinsufficiency.probability","Human.expression.breadth","Mean.Pct","Human.mouse.dN.dS")]))
+mn_x_pctmir = multinom(evo_class~Haploinsufficiency.probability+Human.expression.breadth+Human.mouse.dN.dS+Mean.Pct,data=na.omit(ancX_forlogit[,c("evo_class","Haploinsufficiency.probability","Human.expression.breadth","Mean.Pct","Human.mouse.dN.dS")]))
+mn_x_nophi = multinom(evo_class~Human.expression.breadth+Human.mouse.dN.dS+Mean.Pct,data=na.omit(ancX_forlogit[,c("evo_class","Haploinsufficiency.probability","Human.expression.breadth","Mean.Pct","Human.mouse.dN.dS")]))
 
-ancZ_forlogit = read.table("PATH_TO_SUPPTABLE_S3",sep="\t",fill=TRUE,header = TRUE,stringsAsFactors = FALSE)
+ancZ_forlogit = read.table("PATH_TO_SUPPTABLE_S4",sep="\t",fill=TRUE,header = TRUE,stringsAsFactors = FALSE)
 ancZ_forlogit$evo_class = NA
-ancZ_forlogit[which(ancZ_forlogit$gene %in% subset(ancZ_forlogit,chicken.W=="Y")$gene),"evo_class"] = "ZW"
-ancZ_forlogit[which(ancZ_forlogit$gene %in% subset(ancZ_forlogit,chicken.W=="N")$gene),"evo_class"] = "AncZ"
-mn_z_nomir = multinom(evo_class~Haploinsufficiency.probability+Human.expression.breadth+Chicken-flycatcher.dN/dS,data=na.omit(ancZ_forlogit[,c("evo_class","Haploinsufficiency.probability","Human.expression.breadth","Mean.Pct","Chicken-flycatcher.dN/dS")]))
-mn_z_pctmir = multinom(evo_class~Haploinsufficiency.probability+Human.expression.breadth+Chicken-flycatcher.dN/dS+Mean.Pct,data=na.omit(ancZ_forlogit[,c("evo_class","Haploinsufficiency.probability","Human.expression.breadth","Mean.Pct","Chicken-flycatcher.dN/dS")]))
+ancZ_forlogit[which(ancZ_forlogit$Gene %in% subset(ancZ_forlogit,chicken.W=="Y")$Gene),"evo_class"] = "ZW"
+ancZ_forlogit[which(ancZ_forlogit$Gene %in% subset(ancZ_forlogit,chicken.W=="N")$Gene),"evo_class"] = "AncZ"
+mn_z_nomir = multinom(evo_class~Haploinsufficiency.probability+Human.expression.breadth+Chicken.flycatcher.dN.dS,data=na.omit(ancZ_forlogit[,c("evo_class","Haploinsufficiency.probability","Human.expression.breadth","Mean.Pct","Chicken.flycatcher.dN.dS")]))
+mn_z_pctmir = multinom(evo_class~Haploinsufficiency.probability+Human.expression.breadth+Chicken.flycatcher.dN.dS+Mean.Pct,data=na.omit(ancZ_forlogit[,c("evo_class","Haploinsufficiency.probability","Human.expression.breadth","Mean.Pct","Chicken.flycatcher.dN.dS")]))
+mn_z_nophi = multinom(evo_class~Human.expression.breadth+Chicken.flycatcher.dN.dS+Mean.Pct,data=na.omit(ancZ_forlogit[,c("evo_class","Haploinsufficiency.probability","Human.expression.breadth","Mean.Pct","Chicken.flycatcher.dN.dS")]))
